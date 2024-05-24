2ad68a2468c88cfb3a2050ce71bf7998ea7d4178
HanTongo <eyjtong@gmail.com>
2024-05-24 14:59:45 +0100

    Finalised data points to be collected. Presents in a dataframe.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from requests import get
 from bs4 import BeautifulSoup
+import pandas as pd
 
 # Lists to store the scraped data in
 names = []
@@ -10,8 +11,12 @@ votes = []
 
 # Grabs at least something from the site.
 url = 'https://www.imdb.com/search/title/?title_type=feature&release_date=2003-01-01,2003-12-31'
+
 # Headers used as IMDB doesn't allow the get() without a user agent. 
-headers = {"User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36'}
+# Turns and keeps movie titles in English
+headers = {"Accept-Language": "en-US, en;q=0.5",
+           "User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36'}
+
 # Implemented the headers to grab url items.
 response = get(url, headers=headers)
 html_soup = BeautifulSoup(response.text, 'html.parser')
@@ -23,33 +28,35 @@ movie_containers = html_soup.find_all("li", class_="ipc-metadata-list-summary-it
 for container in movie_containers:
     # If the movie has a Metascore, then extract:
     if container.find("span", class_="metacritic-score-label") is not None:
-        # The name 
+        
+        # Movie Title 
         name = container.h3.text[3:]
         names.append(name)
 
-        # The year 
+        # Movie Year Release
         year = container.find("span", class_="sc-b189961a-8 kLaxqf dli-title-metadata-item")
-        years.append(year)
+        years.append(int(year.text))
 
-        # The IMDB rating
-        
+        # Movie's IMDB rating
         rating_retrieval = container.find("span", class_="ipc-rating-star ipc-rating-star--base ipc-rating-star--imdb ratingGroup--imdb-rating")
-        first_imdb = float(rating_retrieval.text[:3])
-        print(first_imdb)
+        imdb_rating = float(rating_retrieval.text[:3])
+        imdb_ratings.append(imdb_rating)
         
-        # The Metascore
-        first_meta = float(first_movie.find("span", class_="sc-b0901df4-0 bcQdDJ metacritic-score-box").text)
-        print(f"Metascore: {first_meta}")
+        # Movie's Metascore
+        metascore = float(container.find("span", class_="sc-b0901df4-0 bcQdDJ metacritic-score-box").text)
+        metascores.append(metascore)
 
-        # Retrieves IMBb votes
-        first_vote = first_movie.find("span", class_="ipc-rating-star--voteCount")
-        print(first_vote.text[2:-1])
+        # Number of IMBb votes
+        vote_retrieval = container.find("span", class_="ipc-rating-star--voteCount")
+        vote = (vote_retrieval.text[2:-1])
+        votes.append(vote)
 
+# Sets up dataframe to present data
+page_data_df = pd.DataFrame({"Movie": names,
+                        "Year Released": years,
+                        "IMDb": imdb_ratings,
+                        "Metascore": metascores,
+                        "Votes": votes
+                        })
 
-# Retrieves movie's IMDb rating
-
-
-# Retrieves the meta score
-
-
-
+print(page_data_df)
