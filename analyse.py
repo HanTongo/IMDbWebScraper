bbe28e8dd0fee66525ea94093469f424e6a6e00d
HanTongo <eyjtong@gmail.com>
2024-06-03 22:39:14 +0100

    Added letter numbering to the titles

--- analyse.py
+++ analyse.py
@@ -10,17 +10,17 @@ ax1, ax2, ax3 = fig.axes
 
 # Plot 1 consists of a histogram of the IMDb ratings
 ax1.hist(movie_ratings["IMDb"], bins = 10, range = (0,10))
-ax1.set_title("IMDb Rating Distribution")
+ax1.set_title("a) IMDb Rating Distribution")
 
 # Plot 2 consists of a histogram of the Metascore ratings
 ax2.hist(movie_ratings["Metascore"], bins = 10, range = (0,100))
-ax2.set_title("Metascore Distribution")
+ax2.set_title("b) Metascore Distribution")
 
 # Plot 3 consists of two histograms comparing IMDb and Metascore ratings
 ax3.hist(movie_ratings["n_IMDb"], bins = 10, range = (0,100), histtype = "step")
 ax3.hist(movie_ratings["Metascore"], bins = 10, range = (0,100), histtype = "step")
 ax3.legend(["IMDb", "Metascore"], loc = "upper left")
-ax3.set_title("The Two Normalised Distributions")
+ax3.set_title("c) The Two Normalised Distributions")
 
 # Removes the top and right sides of the plot borders
 for ax in fig.axes:
